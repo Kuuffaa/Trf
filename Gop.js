0bfa205eb44d0d3840a742783ba50fbe4d8bf8d2
Kuuffaa <155504556+Kuuffaa@users.noreply.github.com>
2024-03-02 19:13:39 +0100

    Update Gop.js

--- Gop.js
+++ Gop.js
@@ -9,6 +9,6 @@ class Cr {
   }
 }
 
-const myCar = new Car("Foods", 20022);
+const myCar = new Car("Foods", 2022);
 document.getElementById("demo").inrHTML =
 "My car is " + myCar.age() + " years oldman.";
